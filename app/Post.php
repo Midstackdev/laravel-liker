d24f5616799b6740c49f79a44fcc7adee6dd257e
Midstackdev <midstackdev@gmail.com>
2020-01-24 12:21:22 +0000

    Posts

--- app/Post.php
+++ app/Post.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    protected $fillable = [
+    	'body'
+    ];
+
+    public function user()
+    {
+    	return $this->belongsTo(User::class);
+    }
+
+    public function scopeLatestFirst($query)
+    {
+    	return $query->orderBy('created_at', 'desc');
+    }
 }
